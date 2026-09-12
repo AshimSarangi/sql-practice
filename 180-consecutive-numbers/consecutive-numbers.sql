c4c7cdcfa3e958737fe7ece1444d7eca3d34a811
Ashim Kumar Sarangi <ashimsarangi27@gmail.com>
2026-09-12 13:10:22 +0530

    Time: 745 ms (15.32%) | Memory: 0B (100.00%) - LeetSync

---
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -0,0 +1,11 @@
+/* Write your T-SQL query statement below */
+SELECT DISTINCT num as ConsecutiveNums
+ FROM
+(
+    SELECT num,
+    LAG(num,1) OVER (ORDER BY id) as prev1,
+    LAG(num,2) OVER (ORDER BY id) as prev2
+    FROM Logs
+) t
+WHERE num = prev1 AND
+num = prev2
